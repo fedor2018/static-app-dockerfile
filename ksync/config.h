5dedb12b8a0f5216be8b46927fed99edfd0424de
fedor2018 <fedoseev2004@yahoo.com>
2026-07-14 12:46:01 +0300

    add ksynk fix

---
+++ ksync/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* CHANGE THIS! This is the path for the SQLite DB. */
+#define DB_PATH "/config/ksync.sqlite"
+/* Permissions for the SQLite DB. Defaults to 600. See chmod(2) manpages */
+#define DB_PERMS S_IRUSR | S_IWUSR
+/* Usernames/password lengths must be less than or equal to this amount
+   KOReader hashes sync server passwords as md5 *client-side*.
+   Passwords received by ksync (should) always be 32 characters long.
+*/
+#define MAX_USERPASS_LEN 128
+/* Make this 0 to disable registrations */
+#define REGISTRATIONS_ALLOWED 1
+
+#endif
